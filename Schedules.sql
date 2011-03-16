0f46f341615a833dd9a2804bf875b42e0f29ab3e
Schabse Laks <Schabse@GMail.com>
2011-03-16 13:11:05 -0500

    SQL Cleanup
    [git-p4: depot-paths = "//depot/Shul/Applications/Schedules/": change = 3399]

--- Schedules.sql
+++ Schedules.sql
@@ -1,11 +1,8 @@
-DROP TABLE Schedules.ScheduleTimes
-DROP TABLE Schedules.ScheduleDates
-
 CREATE TABLE Schedules.ScheduleDates (
 	ID				UNIQUEIDENTIFIER	ROWGUIDCOL	CONSTRAINT ScheduleDatesKey		PRIMARY KEY DEFAULT newid(),
 	[Date]			DATETIME			NOT NULL,--	CONSTRAINT ScheduleDatesUnique	UNIQUE,
 	Title			NVARCHAR(128)		NOT NULL
-)
+);
 CREATE TABLE Schedules.ScheduleTimes (
 	ID				UNIQUEIDENTIFIER	ROWGUIDCOL	CONSTRAINT ScheduleTimesKey		PRIMARY KEY DEFAULT newid(),
 	CellID			UNIQUEIDENTIFIER	NOT NULL	CONSTRAINT ScheduleTimesForeignKey REFERENCES Schedules.ScheduleDates(ID) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -13,4 +10,4 @@ CREATE TABLE Schedules.ScheduleTimes (
 	[Time]			DATETIME			NOT NULL,
 	IsBold			BIT					NOT NULL	DEFAULT 0,
 	LastModified	DATETIME			NOT NULL	DEFAULT getdate()
-)
+);
