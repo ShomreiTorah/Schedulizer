3d9174cb3f217ff59b1b7d83d38bd8047d1ac9cc
Schabse Laks <Dev@SLaks.net>
2015-06-14 20:34:48 -0400

    Add automatic dumper to detect changes

---
+++ Schedulizer.Dumper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShomreiTorah.Common.Calendar;
+
+namespace ShomreiTorah.Schedules.Dumper {
+	class Program {
+		static void Main(string[] args) {
+			for (var date = new HebrewDate(new DateTime(2015, 1, 1)); date.EnglishDate.Year <= 2020; date++) {
+				var calc = new ScheduleCalculator(date);
+				Console.WriteLine(date.EnglishDate.ToShortDateString()
+								+ "  " + date.EnglishDate.ToLongDateString().PadRight(25)
+								+ "  " + date);
+				Console.WriteLine(calc.CalcTitle());
+				foreach (var time in calc.CalcTimes()) {
+					Console.WriteLine((time.Name + ":").PadRight(10) + time.TimeString + (time.IsBold ? " **" : ""));
+				}
+				Console.WriteLine();
+				Console.WriteLine();
+			}
+			if (!Console.IsOutputRedirected) {
+				Console.ReadKey();
+			}
+		}
+	}
+}
