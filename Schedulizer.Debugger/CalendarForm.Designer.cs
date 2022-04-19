1a034628f4b846430849ba08c19b5613bc43001a
David Laks <david@laksfamily.com>
2022-04-18 21:13:47 -0400

    Add a debug UI for setting breakpoints

---
+++ Schedulizer.Debugger/CalendarForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Schedulizer.Debugger
+{
+	partial class CalendarForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.monthCalendar = new System.Windows.Forms.MonthCalendar();
+			this.SuspendLayout();
+			// 
+			// monthCalendar
+			// 
+			this.monthCalendar.CalendarDimensions = new System.Drawing.Size(2, 2);
+			this.monthCalendar.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.monthCalendar.Location = new System.Drawing.Point(0, 0);
+			this.monthCalendar.Name = "monthCalendar";
+			this.monthCalendar.TabIndex = 0;
+			this.monthCalendar.DateSelected += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar_DateSelected);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(454, 312);
+			this.Controls.Add(this.monthCalendar);
+			this.Name = "Form1";
+			this.Text = "Schedulizer";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MonthCalendar monthCalendar;
+	}
+}
+
---
+++ Schedulizer.Debugger/CalendarForm.cs
@@ -0,0 +1,28 @@
+﻿using ShomreiTorah.Common.Calendar;
+using ShomreiTorah.Schedules;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Schedulizer.Debugger
+{
+	public partial class CalendarForm : Form
+	{
+		public CalendarForm()
+		{
+			InitializeComponent();
+		}
+
+		private void monthCalendar_DateSelected(object sender, DateRangeEventArgs e)
+		{
+			ScheduleCalculator calc = new ScheduleCalculator(new HebrewDate(monthCalendar.SelectionStart));
+			MessageBox.Show($"{calc.CalcTitle()}\n\n{string.Join("\n", calc.CalcTimes().ToList())}");
+		}
+	}
+}
